7943ca3f8c034097f3f9c7e79c60f6659c242c2f
Paavveel <pa.tomilin@gmail.com>
2022-01-21 11:19:54 +0300

    some fix

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,9 +1,42 @@
 import React from 'react';
+// import Swiper core and required modules
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
 
 function Info() {
   return (
-    <div>
-      <h1>INFO</h1>
+    <div className='main-page'>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Autoplay, A11y]}
+        loop={true}
+        slidesPerView={1}
+        speed={1000}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        spaceBetween={30}
+        navigation={true}
+        pagination={{
+          type: 'fraction',
+        }}
+      >
+        <SwiperSlide>
+          <img
+            src='https://via.placeholder.com/2000x2000'
+            alt=''
+            className='slide'
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
