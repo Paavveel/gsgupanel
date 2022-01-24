3b45b5fa2eb358206e9dc389d078d9e3e9fd77f2
Paavveel <pa.tomilin@gmail.com>
2022-01-24 14:43:54 +0300

    fix Structur

--- src/pages/Structur.js
+++ src/pages/Structur.js
@@ -5,6 +5,9 @@ function Structur() {
     <div className='main-page main-container structur'>
       <a href='https://gukolomna.ru/obrazovanie/departments/'>Факультеты</a>
       <a href='https://gukolomna.ru/obrazovanie/kafedry/'>Кафедры</a>
+      <a href='https://gukolomna.ru/obrazovanie/kafedry/'>
+        Цифровой портрет факультетов
+      </a>
     </div>
   );
 }
