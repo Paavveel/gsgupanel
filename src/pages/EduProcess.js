31d6bf6a6151e12e42c913266fbb7a56f8025b48
Paavveel <pa.tomilin@gmail.com>
2022-01-24 12:49:58 +0300

    add eduprocess

--- src/pages/EduProcess.js
+++ src/pages/EduProcess.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 function EduProcess() {
-  return <div className='main-page'></div>;
+  return (
+    <div className='main-page main-container eduprocess'>
+      <a href='https://gukolomna.ru/sveden/eduStandarts/'>
+        Образовательные стандарты
+      </a>
+      <a href='https://gukolomna.ru/sveden/education/'>Учебные планы</a>
+      <a href='https://mega.nz/folder/vB9jDC4A#-009H7qKTxSmKNAjKyYH7g'>
+        График учебного процесса
+      </a>
+      <a href='https://gukolomna.ru/sveden/education/'>
+        Календарный учебный график
+      </a>
+      <a href='https://mega.nz/folder/mdtRCCgZ#unl8cyEEjrA5wXKduqsrsA'>
+        Расписание занятий
+      </a>
+      <a href='https://mega.nz/folder/6N8TiQJJ#5RqUTlH4hxySG2WJv6PHZw'>
+        Расписание сессий
+      </a>
+      <a href='https://gukolomna.ru/studentu/organizatsiya-uchebnogo-protsessa/'>
+        Организация учебного процесса в 2021-2022 учебном году
+      </a>
+    </div>
+  );
 }
 
 export default EduProcess;
