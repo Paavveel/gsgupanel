cb5cb10143a48a0e9484d38a0540f54be39b6e3f
WainDev <wainclown@gmail.com>
2025-11-21 13:53:53 +0300

    update abitur

--- src/pages/Abitur.js
+++ src/pages/Abitur.js
@@ -10,125 +10,146 @@ import "swiper/css/autoplay";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import ab1 from "../img/abitur/ab1.jpg";
-import ab2 from "../img/abitur/ab2.jpg";
+// import ab1 from "../img/abitur/ab1.jpg";
+// import ab2 from "../img/abitur/ab2.jpg";
 
-import ffl1 from "../img/abitur/ffl/1.jpg";
-import ffl2 from "../img/abitur/ffl/2.jpg";
-import ffl3 from "../img/abitur/ffl/3.jpg";
-import ffl4 from "../img/abitur/ffl/4.jpg";
-import ffl5 from "../img/abitur/ffl/5.jpg";
-import ffl6 from "../img/abitur/ffl/6.jpg";
-import ffl7 from "../img/abitur/ffl/7.jpg";
-import ffl8 from "../img/abitur/ffl/8.jpg";
+import ffl1 from "../img/abitur/ffl/1.JPG";
+import ffl2 from "../img/abitur/ffl/2.JPG";
+import ffl3 from "../img/abitur/ffl/3.JPG";
+import ffl4 from "../img/abitur/ffl/4.JPG";
+import ffl5 from "../img/abitur/ffl/5.JPG";
+import ffl6 from "../img/abitur/ffl/6.JPG";
+import ffl7 from "../img/abitur/ffl/7.JPG";
+import ffl8 from "../img/abitur/ffl/8.JPG";
 import ffl9 from "../img/abitur/ffl/9.JPG";
 import ffl10 from "../img/abitur/ffl/10.JPG";
 import ffl11 from "../img/abitur/ffl/11.JPG";
 import ffl12 from "../img/abitur/ffl/12.JPG";
 import ffl13 from "../img/abitur/ffl/13.JPG";
 import ffl14 from "../img/abitur/ffl/14.JPG";
+import ffl15 from "../img/abitur/ffl/15.JPG";
+import ffl16 from "../img/abitur/ffl/16.JPG";
+import ffl17 from "../img/abitur/ffl/17.JPG";
+import ffl18 from "../img/abitur/ffl/18.JPG";
+import ffl19 from "../img/abitur/ffl/19.JPG";
+import ffl20 from "../img/abitur/ffl/20.JPG";
 
-import fius1 from "../img/abitur/fius/1.jpg";
-import fius2 from "../img/abitur/fius/2.jpg";
-import fius3 from "../img/abitur/fius/3.jpg";
-import fius4 from "../img/abitur/fius/4.jpg";
-import fius5 from "../img/abitur/fius/5.jpg";
-import fius6 from "../img/abitur/fius/6.jpg";
-import fius7 from "../img/abitur/fius/7.jpg";
-import fius8 from "../img/abitur/fius/8.jpg";
-import fius9 from "../img/abitur/fius/9.jpg";
-import fius10 from "../img/abitur/fius/10.jpg";
-import fius11 from "../img/abitur/fius/11.jpg";
-import fius12 from "../img/abitur/fius/12.jpg";
-import fius13 from "../img/abitur/fius/13.jpg";
+import fius1 from "../img/abitur/fius/1.JPG";
+import fius2 from "../img/abitur/fius/2.JPG";
+import fius3 from "../img/abitur/fius/3.JPG";
+import fius4 from "../img/abitur/fius/4.JPG";
+import fius5 from "../img/abitur/fius/5.JPG";
+import fius6 from "../img/abitur/fius/6.JPG";
+import fius7 from "../img/abitur/fius/7.JPG";
+import fius8 from "../img/abitur/fius/8.JPG";
+import fius9 from "../img/abitur/fius/9.JPG";
+import fius10 from "../img/abitur/fius/10.JPG";
+import fius11 from "../img/abitur/fius/11.JPG";
+import fius12 from "../img/abitur/fius/12.JPG";
+import fius13 from "../img/abitur/fius/13.JPG";
+import fius14 from "../img/abitur/fius/14.JPG";
+import fius15 from "../img/abitur/fius/15.JPG";
+import fius16 from "../img/abitur/fius/16.JPG";
+import fius17 from "../img/abitur/fius/17.JPG";
 
-import fmfh1 from "../img/abitur/fmfh/1.jpg";
-import fmfh2 from "../img/abitur/fmfh/2.jpg";
-import fmfh3 from "../img/abitur/fmfh/3.jpg";
-import fmfh4 from "../img/abitur/fmfh/4.jpg";
-import fmfh5 from "../img/abitur/fmfh/5.jpg";
-import fmfh6 from "../img/abitur/fmfh/6.jpg";
-import fmfh7 from "../img/abitur/fmfh/7.jpg";
-import fmfh8 from "../img/abitur/fmfh/8.jpg";
+import fmfh1 from "../img/abitur/fmfh/1.JPG";
+import fmfh2 from "../img/abitur/fmfh/2.JPG";
+import fmfh3 from "../img/abitur/fmfh/3.JPG";
+import fmfh4 from "../img/abitur/fmfh/4.JPG";
+import fmfh5 from "../img/abitur/fmfh/5.JPG";
+import fmfh6 from "../img/abitur/fmfh/6.JPG";
+import fmfh7 from "../img/abitur/fmfh/7.JPG";
+import fmfh8 from "../img/abitur/fmfh/8.JPG";
 import fmfh9 from "../img/abitur/fmfh/9.JPG";
 import fmfh10 from "../img/abitur/fmfh/10.JPG";
 import fmfh11 from "../img/abitur/fmfh/11.JPG";
 import fmfh12 from "../img/abitur/fmfh/12.JPG";
+import fmfh13 from "../img/abitur/fmfh/13.JPG";
+import fmfh14 from "../img/abitur/fmfh/14.JPG";
+import fmfh15 from "../img/abitur/fmfh/15.JPG";
+import fmfh16 from "../img/abitur/fmfh/16.JPG";
 
-import pf1 from "../img/abitur/pf/1.jpg";
-import pf2 from "../img/abitur/pf/2.jpg";
-import pf3 from "../img/abitur/pf/3.jpg";
-import pf4 from "../img/abitur/pf/4.jpg";
-import pf5 from "../img/abitur/pf/5.jpg";
-import pf6 from "../img/abitur/pf/6.jpg";
-import pf7 from "../img/abitur/pf/7.jpg";
-import pf8 from "../img/abitur/pf/8.jpg";
-import pf9 from "../img/abitur/pf/9.jpg";
-import pf10 from "../img/abitur/pf/10.jpg";
-import pf11 from "../img/abitur/pf/11.jpg";
+import pf1 from "../img/abitur/pf/1.JPG";
+import pf2 from "../img/abitur/pf/2.JPG";
+import pf3 from "../img/abitur/pf/3.JPG";
+import pf4 from "../img/abitur/pf/4.JPG";
+import pf5 from "../img/abitur/pf/5.JPG";
+import pf6 from "../img/abitur/pf/6.JPG";
+import pf7 from "../img/abitur/pf/7.JPG";
+import pf8 from "../img/abitur/pf/8.JPG";
+import pf9 from "../img/abitur/pf/9.JPG";
+import pf10 from "../img/abitur/pf/10.JPG";
+import pf11 from "../img/abitur/pf/11.JPG";
+import pf12 from "../img/abitur/pf/12.JPG";
 
-import spf1 from "../img/abitur/spf/1.jpg";
-import spf2 from "../img/abitur/spf/2.jpg";
-import spf3 from "../img/abitur/spf/3.jpg";
-import spf4 from "../img/abitur/spf/4.jpg";
-import spf5 from "../img/abitur/spf/5.jpg";
-import spf6 from "../img/abitur/spf/6.jpg";
-import spf7 from "../img/abitur/spf/7.jpg";
+import spf1 from "../img/abitur/spf/1.JPG";
+import spf2 from "../img/abitur/spf/2.JPG";
+import spf3 from "../img/abitur/spf/3.JPG";
+import spf4 from "../img/abitur/spf/4.JPG";
+import spf5 from "../img/abitur/spf/5.JPG";
+import spf6 from "../img/abitur/spf/6.JPG";
+import spf7 from "../img/abitur/spf/7.JPG";
+import spf8 from "../img/abitur/spf/8.JPG";
+import spf9 from "../img/abitur/spf/9.JPG";
 
-import tf1 from "../img/abitur/tf/1.jpg";
-import tf2 from "../img/abitur/tf/2.jpg";
-import tf3 from "../img/abitur/tf/3.jpg";
-import tf4 from "../img/abitur/tf/4.jpg";
-import tf5 from "../img/abitur/tf/5.jpg";
-import tf6 from "../img/abitur/tf/6.jpg";
-import tf7 from "../img/abitur/tf/7.jpg";
-import tf8 from "../img/abitur/tf/8.jpg";
-import tf9 from "../img/abitur/tf/9.jpg";
-import tf10 from "../img/abitur/tf/10.jpg";
-import tf11 from "../img/abitur/tf/11.jpg";
+import tf1 from "../img/abitur/tf/1.JPG";
+import tf2 from "../img/abitur/tf/2.JPG";
+import tf3 from "../img/abitur/tf/3.JPG";
+import tf4 from "../img/abitur/tf/4.JPG";
+import tf5 from "../img/abitur/tf/5.JPG";
+import tf6 from "../img/abitur/tf/6.JPG";
+import tf7 from "../img/abitur/tf/7.JPG";
+import tf8 from "../img/abitur/tf/8.JPG";
+import tf9 from "../img/abitur/tf/9.JPG";
+import tf10 from "../img/abitur/tf/10.JPG";
+import tf11 from "../img/abitur/tf/11.JPG";
 
-import ffkis1 from "../img/abitur/ffkis/1.jpg";
-import ffkis2 from "../img/abitur/ffkis/2.jpg";
-import ffkis3 from "../img/abitur/ffkis/3.jpg";
-import ffkis4 from "../img/abitur/ffkis/4.jpg";
-import ffkis5 from "../img/abitur/ffkis/5.jpg";
-import ffkis6 from "../img/abitur/ffkis/6.jpg";
+import ffkis1 from "../img/abitur/ffkis/1.JPG";
+import ffkis2 from "../img/abitur/ffkis/2.JPG";
+import ffkis3 from "../img/abitur/ffkis/3.JPG";
+import ffkis4 from "../img/abitur/ffkis/4.JPG";
+import ffkis5 from "../img/abitur/ffkis/5.JPG";
+import ffkis6 from "../img/abitur/ffkis/6.JPG";
+import ffkis7 from "../img/abitur/ffkis/7.JPG";
+import ffkis8 from "../img/abitur/ffkis/8.JPG";
+import ffkis9 from "../img/abitur/ffkis/9.JPG";
+import ffkis10 from "../img/abitur/ffkis/10.JPG";
 
-import ff1 from "../img/abitur/ff/1.jpg";
-import ff2 from "../img/abitur/ff/2.jpg";
-import ff3 from "../img/abitur/ff/3.jpg";
-import ff4 from "../img/abitur/ff/4.jpg";
-import ff5 from "../img/abitur/ff/5.jpg";
-import ff6 from "../img/abitur/ff/6.jpg";
-import ff7 from "../img/abitur/ff/7.jpg";
-import ff8 from "../img/abitur/ff/8.jpg";
+import ff1 from "../img/abitur/ff/1.JPG";
+import ff2 from "../img/abitur/ff/2.JPG";
+import ff3 from "../img/abitur/ff/3.JPG";
+import ff4 from "../img/abitur/ff/4.JPG";
+import ff5 from "../img/abitur/ff/5.JPG";
+import ff6 from "../img/abitur/ff/6.JPG";
+import ff7 from "../img/abitur/ff/7.JPG";
+import ff8 from "../img/abitur/ff/8.JPG";
 import ff9 from "../img/abitur/ff/9.JPG";
 
-import ef1 from "../img/abitur/ef/1.jpg";
-import ef2 from "../img/abitur/ef/2.jpg";
-import ef3 from "../img/abitur/ef/3.jpg";
-import ef4 from "../img/abitur/ef/4.jpg";
-import ef5 from "../img/abitur/ef/5.jpg";
-import ef6 from "../img/abitur/ef/6.jpg";
-import ef7 from "../img/abitur/ef/7.jpg";
+import ef1 from "../img/abitur/ef/1.JPG";
+import ef2 from "../img/abitur/ef/2.JPG";
+import ef3 from "../img/abitur/ef/3.JPG";
+import ef4 from "../img/abitur/ef/4.JPG";
+import ef5 from "../img/abitur/ef/5.JPG";
+import ef6 from "../img/abitur/ef/6.JPG";
+import ef7 from "../img/abitur/ef/7.JPG";
 
-import uf1 from "../img/abitur/uf/1.jpg";
-import uf2 from "../img/abitur/uf/2.jpg";
-import uf3 from "../img/abitur/uf/3.jpg";
-import uf4 from "../img/abitur/uf/4.jpg";
-import uf5 from "../img/abitur/uf/5.jpg";
-import uf6 from "../img/abitur/uf/6.jpg";
-import uf7 from "../img/abitur/uf/7.jpg";
-import uf8 from "../img/abitur/uf/8.jpg";
-import uf9 from "../img/abitur/uf/9.jpg";
-import uf10 from "../img/abitur/uf/10.jpg";
-import uf11 from "../img/abitur/uf/11.jpg";
-import uf12 from "../img/abitur/uf/12.jpg";
-import uf13 from "../img/abitur/uf/13.jpg";
+import uf1 from "../img/abitur/uf/1.JPG";
+import uf2 from "../img/abitur/uf/2.JPG";
+import uf3 from "../img/abitur/uf/3.JPG";
+import uf4 from "../img/abitur/uf/4.JPG";
+import uf5 from "../img/abitur/uf/5.JPG";
+import uf6 from "../img/abitur/uf/6.JPG";
+import uf7 from "../img/abitur/uf/7.JPG";
+import uf8 from "../img/abitur/uf/8.JPG";
+import uf9 from "../img/abitur/uf/9.JPG";
+import uf10 from "../img/abitur/uf/10.JPG";
+import uf11 from "../img/abitur/uf/11.JPG";
+import uf12 from "../img/abitur/uf/12.JPG";
+import uf13 from "../img/abitur/uf/13.JPG";
 
 const images = [
-  ab1,
-  ab2,
+  // ab1,
+  // ab2,
   ffl1,
   ffl2,
   ffl3,
@@ -143,6 +164,14 @@ const images = [
   ffl12,
   ffl13,
   ffl14,
+  ffl15,
+  ffl16,
+  ffl17,
+  ffl18,
+  ffl19,
+  ffl20,
+
+
   fius1,
   fius2,
   fius3,
@@ -156,6 +185,11 @@ const images = [
   fius11,
   fius12,
   fius13,
+  fius14,
+  fius15,
+  fius16,
+  fius17,
+
 
   fmfh1,
   fmfh2,
@@ -169,6 +203,13 @@ const images = [
   fmfh10,
   fmfh11,
   fmfh12,
+  fmfh13,
+  fmfh14,
+  fmfh15,
+  fmfh16,
+
+
+
   pf1,
   pf2,
   pf3,
@@ -180,6 +221,7 @@ const images = [
   pf9,
   pf10,
   pf11,
+  pf12,
 
   spf1,
   spf2,
@@ -188,6 +230,11 @@ const images = [
   spf5,
   spf6,
   spf7,
+  spf8,
+  spf9,
+
+
+
   tf1,
   tf2,
   tf3,
@@ -199,12 +246,18 @@ const images = [
   tf9,
   tf10,
   tf11,
+
+
   ffkis1,
   ffkis2,
   ffkis3,
   ffkis4,
   ffkis5,
   ffkis6,
+  ffkis7,
+  ffkis8,
+  ffkis9,
+  ffkis10,
 
   ff1,
   ff2,
@@ -241,7 +294,7 @@ const images = [
 export function Abitur() {
   return (
     <div className="main-page">
-      {/* <Swiper
+      <Swiper
         // install Swiper modules
         modules={[Navigation, Pagination, Autoplay, A11y]}
         loop={true}
@@ -262,8 +315,8 @@ export function Abitur() {
             <img src={img} alt={`abitur${i + 1}`} className="slide" />
           </SwiperSlide>
         ))}
-      </Swiper> */}
-      <h5>в разработке...</h5>
+      </Swiper>
+      {/* <h5>в разработке...</h5> */}
     </div>
   );
 }
